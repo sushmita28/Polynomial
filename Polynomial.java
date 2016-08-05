062202476f6bbc762e514c71742579130584dc68
vvinith94 <vinipani@gmail.com>
2016-08-05 11:24:25 +0530

    all functions added

--- Polynomial.java
+++ Polynomial.java
@@ -1,12 +1,6 @@
-package wordsFreq;
+package frequentNwords;
 
-import java.io.BufferedReader;
-
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.*;
-import java.lang.*;
-import java.io.*;
 
 public class Polynomial {
 	
@@ -17,19 +11,7 @@ public class Polynomial {
 		this.poly1=poly1;
 		this .poly2 = poly2;
 	}
-	
-	public static String split(String poly)
-	{
-		for(int i=0;i<poly.length();i++){
-			
-		}
-		return poly;
-	}
-	public static HashMap<Integer,Double> stringToHash(String poly)
-	{
-		
-	}
-	
+
 	public static String addn(String pol1,String pol2)
 	{
 		Map<Integer,Double> polh1 = stringToHash(pol1);
@@ -46,7 +28,6 @@ public class Polynomial {
 		}	
 		for (Map.Entry<Integer, Double> entry : polh2.entrySet()) {
 		    int key = entry.getKey();
-		    double value1 = entry.getValue();
 		    if(!(result.containsKey(key))){
 		    	result.put(key,polh2.get(key));
 		    }
@@ -55,13 +36,7 @@ public class Polynomial {
 		String res = result.toString();
 		return res;
 	}
-	/*
-	public static String hashToString(HashMap<Integer,Double> poly)
-	{
-		return poly1;
-		
-	}
-	*/
+	
 	public static String subn(String pol1,String pol2)
 	{
 		Map<Integer,Double> polh1 = stringToHash(pol1);
@@ -79,7 +54,6 @@ public class Polynomial {
 		
 		for (Map.Entry<Integer, Double> entry : polh2.entrySet()) {
 		    int key = entry.getKey();
-		    double value1 = entry.getValue();
 		    if(!(result.containsKey(key))){
 		    	result.put(key,(-1)*polh2.get(key));
 		    }
@@ -96,27 +70,20 @@ public class Polynomial {
 		Map<Integer,Double> result = new HashMap<>();
 		
 		for (Map.Entry<Integer, Double> entry : polh1.entrySet()) {
-		    int key = entry.getKey();
+		    int key1 = entry.getKey();
 		    double value1 = entry.getValue();
 		    for (Map.Entry<Integer, Double> entry2 : polh2.entrySet()) {
 			    int key2 = entry2.getKey();
 			    double value2 = entry2.getValue();
-			    val = value1+value2;
-			    if(result.containsKey(key+key2){
-		             double val = 
+			    double val = value1*value2;
+			    if(result.containsKey(key1+key2)){
+		             result.put(key1+key2, result.get(key1+key2)+ val); 
 		            
 		        }
-		        
-		        map.put(;
-			    
+				else{
+					result.put(key1+key2, val);
+				}			    
 			}
-		    
-		    
-		    
-		    if(polh2.containsKey(key)){
-		    	value1 = value1-polh2.get(key);
-		    }
-		    result.put(key,value1);
 		}	
 		
 			
@@ -129,11 +96,11 @@ public class Polynomial {
 		StringBuilder HTMLcode = new StringBuilder("");
 
 		HTMLcode.append("<HTML>\n");
-		HTMLcode.append("<HEAD><\HEAD>\n");
+		HTMLcode.append("<HEAD><\\HEAD>\n");
 		HTMLcode.append("<BODY>\n");
 		HTMLcode.append(resultString);
-		HTMLcode.append("\n<\BODY>\n");
-		HTMLCode.append("<\HTML>\n");
+		HTMLcode.append("\n<\\BODY>\n");
+		HTMLcode.append("<\\HTML>\n");
 
 		return (HTMLcode.toString());
 		}
@@ -142,57 +109,52 @@ public class Polynomial {
 
 		StringBuilder LATEXcode = new StringBuilder("");
 
-		LATEXcode.append("\begin{document}\n");
-		LATEXcode.append("\begin{abstract}\n");
+		LATEXcode.append("\\begin{document}\n");
+		LATEXcode.append("\\begin{abstract}\n");
 		LATEXcode.append("Basic Arithmetic Operations performed on Polynomials\n");
-		LATEXcode.append("\end{abstract}\n");
-		LATEXcode.append(resultString);
+		LATEXcode.append("\\end{abstract}\n");
+		LATEXcode.append("$"+resultString+"$");
 		LATEXcode.append("\n\n");
-		LATEXCode.append("\end{document}\n");
+		LATEXcode.append("\\end{document}\n");
 
 		return (LATEXcode.toString());
 		}
 
+		public static HashMap<Integer,Double> stringToHash(String poly)
+		{
+			String replace=poly.replace("-", "+-");
+		    String[] splitString = (replace.split("\\+"));
+		    String[] splithash = null;
+		     for ( int i = 0; i<splitString.length ; i++) {
+		    	 
+		         if(splitString[i].indexOf('x')==-1)
+		         {
+		        	 splitString[i]=splitString[i]+"x^0";
+		         }
+		     }
+		     String str = String.join("x^", splitString);
+		     splithash=str.split("[x]\\^");
+
+		     HashMap<Integer,Double> hm = new HashMap<Integer,Double>();
+		     for(int i=0 ; i < splithash.length ; i = i + 2) {
+		    	 hm.put(Integer.parseInt(splithash[i+1]), Double.parseDouble(splithash[i]));
+		     }
+		     return hm;
+		}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Polynomial p1=new Polynomial("2x^5+2.8x^3","2x^5+2.7x^1");
-			
-		addn(p1.poly1,p1.poly2);
-		subn(p1.poly1,p1.poly2);
-		muln(p1.poly1,p1.poly2);
+		Polynomial p1=new Polynomial("2x^5+2x^2-3x^1+4","2x^3+2x^2-3x^1+4");
+		System.out.println(addn(p1.poly1,p1.poly2));
+		System.out.println(subn(p1.poly1,p1.poly2));
+		System.out.println(muln(p1.poly1,p1.poly2));
+		System.out.println(p1.toLATEX("2x^5+2x^2-3x^1+4"));
+		System.out.println(p1.toHTML("2x^5+2x^2-3x^1+4"));
 	}
 
 
 }
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-public class Polynomial {
-	
-	String polynomial;
-	public static void stringToHash(String poly)
-	{
-		String replace=poly.replace("-", "+-");
-	    System.out.println(replace);
-	    String[] splitString = (replace.split("\\+"));
-	    
-	     for (String string : splitString) {
-	    	 System.out.println(string);
-	         int length=string.length();
-	         if(string.indexOf('x')>=0)
-	         {
-	        	 string=string+"x^0";
-	         }
-	         String[] splithash=string.split("[x]\\^");
-	         for(String string1:splithash)
-	         System.out.println(string1);
-	     }
-	}
-	public static void main(String args[]){
-		stringToHash("2x^5+2x^1-3x^1+4");
-	
-	}
-}
+
+
 
